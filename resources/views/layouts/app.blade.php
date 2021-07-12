98f0eabdeef222e4220de178637228792b561601
oyedolacode <segunzyshegszy12@gmail.com>
2021-07-12 12:54:09 +0100

    sidebar updated

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@yield('title')</title>
+       
+        <!-- Fonts -->
+        <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+         
+        <!-- Custom fonts for this template -->
+        <link href="{{ asset('fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        
+        <!-- Custom styles for this template -->
+       
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body>
+    
+        @yield('pagecontent')
+
+        <script src="{{ asset('bootstrap/css/bootstrap.min.css') }}"></script>
+    </body>
+</html>
